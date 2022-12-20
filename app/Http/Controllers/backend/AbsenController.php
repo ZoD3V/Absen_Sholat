8d3dc4e5bcfcad82858d53697b14a9c3808f99cc
lastorder <adzkiyadzikrullah@gmail.com>
2022-12-20 11:51:18 +0700

    create

--- app/Http/Controllers/backend/AbsenController.php
+++ app/Http/Controllers/backend/AbsenController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\backend;
 
 use App\Http\Controllers\Controller;
+use App\Models\AbsensiModel;
 use Illuminate\Http\Request;
 
 class AbsenController extends Controller
@@ -12,6 +13,20 @@ class AbsenController extends Controller
         return view('backend.absen.index');
     }
 
-    public function 
+    public function create_process(Request $request)
+    {
+        request()->validate([
+            'nama'         => 'required',
+            'nama_sholat'   => 'required'
+        ]);
 
+        AbsensiModel::create([
+            'title'         => $request->nama,
+            'nama_sholat'   => $request->nama_sholat
+        ]);
+
+        return redirect()
+            ->route('backend.absen.index')
+            ->with('success', 'Item Created Successfully');
+    }
 }
